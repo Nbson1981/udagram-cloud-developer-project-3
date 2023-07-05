72530131076f8b9c9da4ae4a6fa36356e4275246
Nguyen Ba Son <nguyenbason@windowslive.com>
2023-07-06 01:39:30 +0700

    update repo that build succeed

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,8 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9828fdb38bac47af9bce138510a76be-976018890.us-west-2.elb.amazonaws.com:8080/api/v0'
+  //apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
